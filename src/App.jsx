621e770b468e7907eea1e0bf495c8bac624ba000
Andrew <thum14252@gmail.com>
2024-04-05 15:17:40 +0630

    about

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import About from "./components/About/About"
 import Header from "./components/Header/Header"
 import Hero from "./components/Hero/Hero"
 import Step from "./components/Step/Step"
@@ -12,6 +13,7 @@ function App() {
     <div className=" xl:bg-grid xl:bg-center xl:bg-repeat-y fixed top-0 bottom-0 left-0 right-0 z-10" ></div>
     <Hero/>
     <Step/>
+    <About/>
 <div className=" h-[3000px]">
 
 </div>
---
+++ src/components/About/About.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import AboutImg from "../../assets/about/img.png"
+
+const About = () => {
+  return (
+    <section className='about mt-[80px] lg:mt-[200px] relative z-20'>
+    <div className='container mx-auto xl:px-1'>
+        <div className='flex flex-col text-center xl:text-left xl:flex-row justify-between items-center gap-8 xl:gap-[77px]'>
+            <div className='flex-1 order-2 xl:order-none'>
+                <h2 className='font-primary text-[48px] lg:text-[60px] leading-none my-9 xl:mb-[80px]'>We Create The Art Of Stylish Living Stylishly</h2>
+
+
+                <p className='xl:mt-[80px] my-6 text-2xl'>It is a long established fact that a reader will be distracted by the of readable content of a page when lookings at its layouts the points of using that it has a more-or-less normal.</p>
+
+                <div className='flex items-center justify-center xl:justify-start gap-5 xl:mt-12'>
+                    <div className=' bg-accent/15 w-[93px] h-[93px] rounded-full flex justify-center items-center'>
+                        <i className="ri-phone-fill text-accent text-4xl"></i>
+                    </div>
+                    <div className='text-left'>
+                    <div className=' text-2xl font-bold'>09234 2345 45</div>
+                    <div>Call Us Anytime</div>
+                    </div>
+                </div>
+                <button className='btn btn-primary'>Get Free Estimation
+                <i className="ri-arrow-right-line text-accent"></i></button>
+            </div>
+            <div className='about__img order-1 xl:order-none max-w-[453px] mx-auto xl:max-w-none xl:mx-0'>
+                <img src={AboutImg}/>
+                </div>
+        </div>
+    </div>
+</section>
+  )
+}
+
+export default About
