298c237104b43f793418ab980babe13ccece4113
Andrew <thum14252@gmail.com>
2024-04-05 12:42:18 +0630

    hero components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-
+import Header from "./components/Header"
+import Hero from "./components/Hero/Hero"
 
 function App() {
 
 
   return (
-    <>
-   <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+   <>
+   <Header/>
+   <main className=" max-w-[1920px] mx-auto bg-white overflow-hidden">
+    <div className=" xl:bg-grid xl:bg-center xl:bg-repeat-y fixed top-0 bottom-0 left-0 right-0 z-10" ></div>
+    <Hero/>
+<div className=" h-[3000px]">
+
+</div>
+   </main>
+   </>
   )
 }
 
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./Hero.css";
+
+
+const Hero = () => {
+  return (
+    <section className="hero h-[640px] xl:h-[840px] bg-hero bg-center lg:bg-cover bg-no-repeat bg-fixed xl:rounded-bl-[290px] relative z-20" >
+<div className="container mx-auto h-full flex items-center justify-center xl:justify-start">
+  <div className='hero__text w-[567px] fle flex-col items-center text-center xl:text-left lg:items-start'>
+    <h1 className='text-[50px] lg:text-[66px] leading-none font-primary mb-9'>Let Your Home Be Unique</h1>
+    <p className='mb-8'>
+    There are many variations of the passages of lorem Ipsum from available,variations of the passages.
+    </p>
+    <button className='btn btn-primary'>
+    Get free estimation
+    </button>
+  </div>
+</div>
+</section>
+  )
+}
+
+export default Hero
